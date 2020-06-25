6ebcd4d8c8154c4fd3f565e7ac88fd801979dacd
Anushka0805 <44258149+Anushka0805@users.noreply.github.com>
2020-06-26 01:42:15 +0530

    Update inf.py

--- inf.py
+++ inf.py
@@ -4,6 +4,6 @@ content_img, style_img = ('images/content1.jpg','images/style1.jpg')
 
 nst=Neural_Style(num_steps=300)
 
-output = nst.run_style_transfer(content_img, style_img)
+output = nst.run_style_transfer(style_img, content_img)
 
 nst.imshow(output)
